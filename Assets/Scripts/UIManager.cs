b2aa7e911259e82724e67b857f56e1943fd63d91
shupcher <shupcher@gmail.com>
2025-05-30 13:32:29 -0500

    Update UIManager.cs

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,15 +1,26 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     private BuildingPlacer _buildingPlacer;
-
     public Transform buildingMenu;
     public GameObject buildingButtonPrefab;
+    public Transform resourcesUIParent;
+    public GameObject gameResourceDisplayPrefab;
+
+    private Dictionary<string, Text> _resourceTexts;
 
     private void Awake()
     {
+        _resourceTexts = new Dictionary<string, Text>();
+        foreach (KeyValuePair<string, GameResource> pair in Globals.GAME_RESOURCES)
+        {
+            GameObject display = Instantiate(gameResourceDisplayPrefab, resourcesUIParent);
+            display.name = pair.Key;
+        }
+
         _buildingPlacer = GetComponent<BuildingPlacer>();
 
         // create buttons for each building type
@@ -28,7 +39,7 @@ public class UIManager : MonoBehaviour
 
     private void _AddBuildingButtonListener(Button b, int i)
     {
-//Outside for loop above because as the for loop iterates, the value i would update. We need to call the method in the loop instead so we only get the value of i at the time it is called
+        //Outside for loop above because as the for loop iterates, the value i would update. We need to call the method in the loop instead so we only get the value of i at the time it is called
         b.onClick.AddListener(() => _buildingPlacer.SelectPlacedBuilding(i));
     }
 }
