6b008048d22440b0899853b6186bdfa08ec9358f
shupcher <shupcher@gmail.com>
2025-06-13 14:04:35 -0500

    Update UnitManager.cs

--- Assets/Scripts/UnitManager.cs
+++ Assets/Scripts/UnitManager.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UnitManager : MonoBehaviour
 {
 //Since BuildingManager inherits from UnitManager, selectionCircle needs to be attached to the BuildingManager script in the inspector.
     public GameObject selectionCircle;
+    public InputActionAsset actions;
+    private InputAction _selectUnitsAction;
 
+    private void Start()
+    {
+      _selectUnitsAction = actions.FindActionMap("UI").FindAction("Click");
+    }
     public void Select()
     {
         if (Globals.SELECTED_UNITS.Contains(this)) return;
