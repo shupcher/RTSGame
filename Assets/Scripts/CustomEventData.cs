40bbdc8efb619b6b0dc1e0815f36d8c4efce0345
shupcher <shupcher@gmail.com>
2025-06-23 13:27:06 -0500

    Created Event Manager, Custom Event Data
    Created Event Manager and Event Data scripts. Need to review vs ebook.

---
+++ Assets/Scripts/CustomEventData.cs
@@ -0,0 +1,15 @@
+
+using UnityEngine.Events;
+
+public class CustomEventData
+{
+    public BuildingData buildingData;
+
+    public CustomEventData(BuildingData buildingData)
+    {
+        this.buildingData = buildingData;
+    }
+}
+
+[System.Serializable]
+public class CustomEvent : UnityEvent<CustomEventData> {}
