a624d76d41675f4188849266acd663bdcd9af860
shupcher <shupcher@gmail.com>
2025-06-24 15:03:14 -0500

    New Input Manager and Callbacks
    Converted OnMouseDown to new input system. When clicking on a unit, it is selected. However, when clicking and holding, the unit gets unselected until I release the mouse button again.

--- Assets/Scripts/UnitManager.cs
+++ Assets/Scripts/UnitManager.cs
@@ -1,15 +1,45 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UnitManager : MonoBehaviour
 {
     //Since BuildingManager inherits from UnitManager, selectionCircle needs to be attached to the BuildingManager script in the inspector
     public GameObject selectionCircle;
+    private DefaultControls _defaultControls;
+    private Ray _ray;
+    private RaycastHit _raycastHit;
 
-    private void OnMouseDown()
+    private void Awake()
     {
-        if (IsActive())
-            Select(true);
+        _defaultControls = new DefaultControls();
+    }
+
+    private void Update()
+    {
+        _ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+    }
+
+    private void OnEnable()
+    {
+        _defaultControls.UI.Click.canceled += OnClick;
+        _defaultControls.UI.Click.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _defaultControls.UI.Click.Disable();
+        _defaultControls.UI.Click.canceled -= OnClick;
+    }
+    void OnClick(InputAction.CallbackContext context)
+    {
+        if (Physics.Raycast(_ray, out _raycastHit, 1000f) &&
+        _raycastHit.collider.gameObject == gameObject)
+        {
+            Debug.Log("Unit clicked: " + gameObject.name);
+            if (IsActive())
+                Select(true);
+        }
     }
 
     protected virtual bool IsActive()
@@ -19,6 +49,7 @@ public class UnitManager : MonoBehaviour
     public void Select() { Select(false); }
     public void Select(bool clearSelection)
     {
+        Debug.Log("Selecting unit: " + gameObject.name);
         if (Globals.SELECTED_UNITS.Contains(this)) return;
         if (clearSelection)
         {
